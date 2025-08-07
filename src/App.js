f6e786464adfa5f8f995aa3f674c2de3b8cd80bf
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-07 16:04:09 +0530

    Basic Code SetUp using Parcel.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () =>{
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout/>);
+
