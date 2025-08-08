eff2152fdd4f6f32ca4a69ed255545ad559868c9
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-08 14:22:03 +0530

    useState hook used to display cards dynamically and added filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from 'react';
+
+ 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={()=>{
+          let filterRestaurants = listOfRestaurants.filter((res)=> res.data.avgRating>4);
+          setListOfRestaurants(filterRestaurants);
+          }}
+         
+        >
+          Top Rated Restaurents
+        </button>
+        </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
